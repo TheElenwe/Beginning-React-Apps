c470d807ec8c078204fac41fb67b4b1e56d79a99
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2022-12-15 15:27:18 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import React, { useEffect, useState } from "react";
-
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
-import useHttp from "./hooks/use-http";
+import BasicForm from './components/BasicForm';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
-
-  useEffect(() => {
-    const transformTasks = (task0bj) => {
-      const loadedTasks = [];
-
-      for (const taskKey in task0bj) {
-        loadedTasks.push({ id: taskKey, text: task0bj[taskKey].text });
-      }
-      setTasks(loadedTasks);
-    };
-
-    fetchTasks(
-      {
-        url: "https://react-http-dc8ba-default-rtdb.firebaseio.com/tasks.json",
-      },
-      transformTasks
-    );
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <BasicForm />
+    </div>
   );
 }
 
